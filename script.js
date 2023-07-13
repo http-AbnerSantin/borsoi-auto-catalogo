dab11b6ba02c160e434336073b838c964799333e
http-AbnerSantin <abner.santin11@gmail.com>
2023-07-13 16:23:04 -0300

    ultimo commit dia 13/07

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ btnMobile.addEventListener('click', () => {
     const menu = document.querySelector('#menu')
     menu.classList.toggle('ativo')
 })
+
+
+//RELOAD DA PÁGINA
+
+const logo = document.querySelector('#logoMenu')
+
+logo.addEventListener('click', () => {
+    location.reload()
+})
+
