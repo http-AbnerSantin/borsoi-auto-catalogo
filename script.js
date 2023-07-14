08041aa97826fc14f23baf7ce07da7a46abbd7be
http-AbnerSantin <abner.santin11@gmail.com>
2023-07-14 12:59:09 -0300

    fazendo um outside no menu do desktop

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-const btnMobile = document.querySelector('#btn-mobile')
+function menuMobile(e) {
+    const btnMobile = document.querySelector('#btn-mobile')
+    
+    if(btnMobile) {
+        btnMobile.addEventListener('click', () => {
+            const menu = document.querySelector('#menu')
+            menu.classList.toggle('ativo')
+        })
+        outside()
+    }
+}
 
-btnMobile.addEventListener('click', () => {
-    const menu = document.querySelector('#menu')
-    menu.classList.toggle('ativo')
-})
+menuMobile()
+
+
+//outside event
+
+function outside(callback) {
+    const html = document.documentElement;
+    console.log(callback)
+
+    function handleOutsideClick(event) {
+        if(event.target === html) {
+            menu.classList.remove('ativo')
+        }
+    }
+    html.addEventListener('click', handleOutsideClick)
+}
+
+
+
+
+
 
 
 //RELOAD DA PÁGINA
